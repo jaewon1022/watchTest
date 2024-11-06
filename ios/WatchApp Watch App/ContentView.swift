2124edd28f70178f1ac61a64074ebe5d7e4afa86
jaewon1022 <andyjaewon@naver.com>
2024-11-06 17:42:34 +0900

    WatchApp 타겟 추가

---
+++ ios/WatchApp Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WatchApp Watch App
+//
+//  Created by mac on 2024/11/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios/WatchApp Watch App/WatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchAppApp.swift
+//  WatchApp Watch App
+//
+//  Created by mac on 2024/11/06.
+//
+
+import SwiftUI
+
+@main
+struct WatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
